222bd26352f1f329359fc29ec994a683245aa246
dbarzin <dbarzin@gmail.com>
2020-12-14 17:51:39 +0100

    add migration

---
+++ database/migrations/2020_12_10_124226_add_disk_to_logical_servers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDiskToLogicalServersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('logical_servers', function (Blueprint $table) {
+            //
+            $table->integer('disk')->nullable();                        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('logical_servers', function (Blueprint $table) {
+            //
+        });
+    }
+}
