bf67a25f973961f902571364eef45e2db5fb01fa
dbarzin <dbarzin@gmail.com>
2021-05-19 18:55:49 +0200

    tests non régression

--- tests/Browser/DnsserverTest.php
+++ tests/Browser/DnsserverTest.php
@@ -2,19 +2,67 @@
 
 namespace Tests\Browser;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
-class DnsserverTest extends DuskTestCase
+class DnsServerTest extends DuskTestCase
 {
     public function testIndex()
     {
+
         $admin = \App\User::find(1);
-        $this->browse(function (Browser $browser) use ($admin) {
-            $browser->loginAs($admin);
-            $browser->visit(route('admin.dnsservers.index'));
-            $browser->assertRouteIs('admin.dnsservers.index');
+        retry($times = 5,  function () use ($admin) {
+            $this->browse(function (Browser $browser) use ($admin) {
+                $browser->loginAs($admin);
+                $browser->visit(route('admin.dnsservers.index'));
+                $browser->waitForText("Mercator");
+                $browser->assertRouteIs('admin.dnsservers.index');
+            });
         });
     }
+
+    public function testView()
+    {
+        $admin = \App\User::find(1);
+		$data = \DB::table('dnsservers')->first();
+		if ($data!=null) 
+        retry($times = 5,  function () use ($admin,$data) {
+            $this->browse(function (Browser $browser) use ($admin,$data) {
+                $browser->loginAs($admin);
+                $browser->visit("/admin/dnsservers/" . $data->id);
+                $browser->waitForText("Mercator");
+                $browser->assertPathIs("/admin/dnsservers/" . $data->id);
+                $browser->assertSee($data->name);
+            });
+        });
+    }
+
+    public function testEdit()
+    {
+        $admin = \App\User::find(1);
+		$data = \DB::table('dnsservers')->first();
+		if ($data!=null) 
+        retry($times = 5,  function () use ($admin,$data) {
+            $this->browse(function (Browser $browser) use ($admin,$data) {
+                $browser->loginAs($admin);
+                $browser->visit("/admin/dnsservers/" . $data->id . "/edit");
+                $browser->waitForText("Mercator");
+                $browser->assertPathIs("/admin/dnsservers/" . $data->id . "/edit");
+            });
+        });
+    }
+
+    public function testCreate()
+    {
+        $admin = \App\User::find(1);
+        retry($times = 5,  function () use ($admin) {
+            $this->browse(function (Browser $browser) use ($admin) {
+                $browser->loginAs($admin);
+                $browser->visit("/admin/dnsservers/create");
+                $browser->waitForText("Mercator");
+                $browser->assertPathIs("/admin/dnsservers/create");
+            });        
+        });
+    }
+
 }
--- tests/Browser/ForestAdTest.php
+++ tests/Browser/ForestAdTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Browser;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
@@ -10,11 +9,60 @@ class ForestAdTest extends DuskTestCase
 {
     public function testIndex()
     {
+
         $admin = \App\User::find(1);
-        $this->browse(function (Browser $browser) use ($admin) {
-            $browser->loginAs($admin);
-            $browser->visit(route('admin.forest-ads.index'));
-            $browser->assertRouteIs('admin.forest-ads.index');
+        retry($times = 5,  function () use ($admin) {
+            $this->browse(function (Browser $browser) use ($admin) {
+                $browser->loginAs($admin);
+                $browser->visit(route('admin.forest-ads.index'));
+                $browser->waitForText("Mercator");
+                $browser->assertRouteIs('admin.forest-ads.index');
+            });
         });
     }
+
+    public function testView()
+    {
+        $admin = \App\User::find(1);
+		$data = \DB::table('forest_ads')->first();
+		if ($data!=null) 
+        retry($times = 5,  function () use ($admin,$data) {
+            $this->browse(function (Browser $browser) use ($admin,$data) {
+                $browser->loginAs($admin);
+                $browser->visit("/admin/forest-ads/" . $data->id);
+                $browser->waitForText("Mercator");
+                $browser->assertPathIs("/admin/forest-ads/" . $data->id);
+                $browser->assertSee($data->name);
+            });
+        });
+    }
+
+    public function testEdit()
+    {
+        $admin = \App\User::find(1);
+		$data = \DB::table('forest_ads')->first();
+		if ($data!=null) 
+        retry($times = 5,  function () use ($admin,$data) {
+            $this->browse(function (Browser $browser) use ($admin,$data) {
+                $browser->loginAs($admin);
+                $browser->visit("/admin/forest-ads/" . $data->id . "/edit");
+                $browser->waitForText("Mercator");
+                $browser->assertPathIs("/admin/forest-ads/" . $data->id . "/edit");
+            });
+        });
+    }
+
+    public function testCreate()
+    {
+        $admin = \App\User::find(1);
+        retry($times = 5,  function () use ($admin) {
+            $this->browse(function (Browser $browser) use ($admin) {
+                $browser->loginAs($admin);
+                $browser->visit("/admin/forest-ads/create");
+                $browser->waitForText("Mercator");
+                $browser->assertPathIs("/admin/forest-ads/create");
+            });        
+        });
+    }
+
 }
