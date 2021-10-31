1b5c5b36e0aa091b7941068af2162763bcd72ac2
dbarzin <dbarzin@gmail.com>
2021-10-31 11:24:18 +0100

    i18n

--- resources/views/doc/about.blade.php
+++ resources/views/doc/about.blade.php
@@ -6,46 +6,19 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    About
+                    {{ trans("doc.about.title")}}
                 </div>
                 <div class="card-body">
-
                   <div class="row">
                     <div class="col-3" style="min-width: 460px;">
                         <img src="/images/mercator.jpg">
                     </div>
                     <div class="col-5">
-
-<p>
-Mercator est une application Web permettant de gérer la cartographie d’un système d’information comme décrit 
-dans le <a href="https://www.ssi.gouv.fr/guide/cartographie-du-systeme-dinformation/">Guide pour la cartographie du Système d’information</a> 
-de l’<a href="https://www.ssi.gouv.fr/">ANSSI</a>. Les sources de l'application sont disponibles sous <a href="https://www.github.com/dbarzin/mercator">Github</a>.
-</p>
-<p>
-Gérard Mercator, né le 5 mars 1512 à Rupelmonde et mort le 2 décembre 1594 à Duisbourg, 
-est un mathématicien, géographe et cartographe flamand, inventeur de la projection
-cartographique qui porte son nom. 
-</p>
-<p>
-La projection Mercator est une projection cartographique de la Terre, 
-dite «cylindrique», tangente à l'équateur du globe terrestre sur une carte plane. 
-Elle s'est imposée comme le planisphère de référence dans le monde grâce à sa précision pour 
-les voyages marins. Ce n'est pas, stricto sensu, une projection centrale : le point de latitude 
-φ n'est pas envoyé, comme on pourrait 
-s'y attendre, sur un point d'ordonnée proportionnelle à tan(φ) mais sur un point d'ordonnée 
-proportionnelle à ln[tan(φ/2 + π/4)].
-</p>
-<p>
-La projection Mercator est une projection conforme, c’est-à-dire qu'elle conserve les angles. 
-Elle a cependant pour effet des déformations sur les distances et les aires. En effet, 
-une distorsion s’accroît au fur et à mesure de l'éloignement de l'équateur vers les pôles. 
-Une carte de Mercator ne peut ainsi couvrir les pôles : ils seraient infiniment grands. 
-Cela a par exemple pour conséquence la vision d'une égalité de surface entre le Groenland 
-et l'Afrique alors que cette dernière est 14 fois plus grande. 
-</p>  
-<p>
-  src: <a href="https://fr.wikipedia.org/wiki/G%C3%A9rard_Mercator">Wikipedia</a>
-</p>
+                        <p> {!! trans("doc.about.p1") !!} </p>
+                        <p> {!! trans("doc.about.p2") !!} </p>
+                        <p> {!! trans("doc.about.p3") !!} </p>
+                        <p> {!! trans("doc.about.p4") !!} </p>
+                        <p> {!! trans("doc.about.p5") !!} </p>
                     </div>
                   </div>
               </div>
@@ -53,7 +26,6 @@ et l'Afrique alors que cette dernière est 14 fois plus grande.
       </div>
   </div>
 </div>
-
 @endsection
 
 @section('scripts')
--- resources/views/doc/guide.blade.php
+++ resources/views/doc/guide.blade.php
@@ -6,26 +6,15 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    Guide
+                    {{ trans("doc.guide.title") }}
                 </div>
                 <div class="card-body">
-
                   <div class="row">
                     <div class="col-6" style="min-width: 460px;">
-
-<p>
-Les attaques informatiques interviennent dans des environnements en constante évolution. Pour répondre à ces enjeux, il est nécessaire de mettre en place une approche globale de gestion des risques au sein de l’organisation. 
-</p>
-
-<p>
-La cartographie du Système d’Information permet d’avoir une vue globale de l’ensemble des éléments qui constituent le système d’information pour d’obtenir une meilleure lisibilité, et donc un meilleur contrôle. 
-</p>
-<p>
-L’élaboration d’une cartographie participe à la protection, à la défense et à la résilience du système d’information. C’est un outil indispensable à la maitrise de son système d’information (SI) et est une obligation pour les Opérateurs d’importance vitale (OIV) et qui s’intègre dans une démarche globale de gestion des risques.
-</p>
-<p>
-Le guide « <a href="https://www.ssi.gouv.fr/entreprise/guide/cartographie-du-systeme-dinformation/" target="new">Cartographie du système d’information – Guide d’élaboration en cinq étapes</a>" publié par l’ANSSI décrit dans la mise en place de cette démarche.
-</p>
+                        <p>{!! trans("doc.guide.p1") !!}</p>
+                        <p>{!! trans("doc.guide.p2") !!}</p>
+                        <p>{!! trans("doc.guide.p3") !!}</p>
+                        <p>{!! trans("doc.guide.p4") !!}</p>
                     </div>
                   </div>
               </div>
@@ -33,7 +22,6 @@ Le guide « <a href="https://www.ssi.gouv.fr/entreprise/guide/cartographie-du-s
       </div>
   </div>
 </div>
-
 @endsection
 
 @section('scripts')
