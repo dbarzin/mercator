dbfdb0b2af530d94ac01ae03349b706c8c54e725
dbarzin <dbarzin@gmail.com>
2022-04-12 23:38:38 +0200

    fix naming issue

--- database/migrations/2022_01_28_150919_add_guid_domain_columns_to_users_table.php
+++ database/migrations/2022_01_28_150919_add_guid_domain_columns_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddGuidDomainColumnsToUserTable extends Migration
+class AddGuidDomainColumnsToUsersTable extends Migration
 {
     /**
      * Run the migrations.
