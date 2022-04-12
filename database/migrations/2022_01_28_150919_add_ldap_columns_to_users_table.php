93596b81ba6598a27c6bf95fc565ee580cd1f540
dbarzin <dbarzin@gmail.com>
2022-04-12 23:23:30 +0200

    update databa

--- database/migrations/2022_01_28_150919_add_ldap_columns_to_users_table.php
+++ database/migrations/2022_01_28_150919_add_ldap_columns_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class MercatorAddLdapColumnsToUsersTable extends Migration
+class AddLdapColumnsToUserTable extends Migration
 {
     /**
      * Run the migrations.
