e60db28e8d53fb3d8dbe7ad6b74ebe1fa16dce43
dbarzin <dbarzin@gmail.com>
2022-04-12 22:34:31 +0200

    fix name conflict

--- database/migrations/2022_01_28_150919_add_ldap_columns_to_users_table.php
+++ database/migrations/2022_01_28_150919_add_ldap_columns_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddLdapColumnsToUsersTable extends Migration
+class MercatorAddLdapColumnsToUsersTable extends Migration
 {
     /**
      * Run the migrations.
