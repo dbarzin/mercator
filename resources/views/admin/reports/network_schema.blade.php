51f5f6d78c341ea18b61580824c6bab5c95514b4
dbarzin <dbarzin@gmail.com>
2023-01-12 20:24:59 +0100

    add links in view

--- resources/views/admin/reports/network_schema.blade.php
+++ resources/views/admin/reports/network_schema.blade.php
@@ -6,7 +6,7 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    {{ trans("cruds.menu.physical_infrastructure.title") }}
+                    {{ trans("cruds.menu.network_schema.title") }}
                 </div>
 
                 <div class="card-body">
