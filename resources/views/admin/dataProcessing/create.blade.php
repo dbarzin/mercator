5ebd4aed296505e03b271910757d69dc263f012a
Didier <dbarzin@gmail.com>
2023-05-10 16:58:46 +0200

    max length

--- resources/views/admin/dataProcessing/create.blade.php
+++ resources/views/admin/dataProcessing/create.blade.php
@@ -11,7 +11,7 @@
             @csrf
             <div class="form-group">
                 <label class="required" for="name">{{ trans('cruds.dataProcessing.fields.name') }}</label>
-                <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', '') }}" required>
+                <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" maxlength=32 value="{{ old('name', '') }}" required>
                 @if($errors->has('name'))
                     <div class="invalid-feedback">
                         {{ $errors->first('name') }}
