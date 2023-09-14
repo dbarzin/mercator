d7466effcc5a5cb500ce6dc7b86ba3a2c5e334fb
Didier <dbarzin@gmail.com>
2023-09-14 19:55:44 +0200

    add role caching

--- app/User.php
+++ app/User.php
@@ -17,47 +17,6 @@ use LdapRecord\Laravel\Auth\LdapAuthenticatable;
 /**
  * App\User
  *
- * @property int $id
- * @property string|null $name
- * @property string|null $email
- * @property \Illuminate\Support\Carbon|null $email_verified_at
- * @property string|null $password
- * @property string|null $remember_token
- * @property int|null $granularity
- * @property string|null $language
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property \Illuminate\Support\Carbon|null $deleted_at
- *
- * @property-read \Illuminate\Database\Eloquent\Collection|array<\Laravel\Passport\Client> $clients
- * @property-read int|null $clients_count
- * @property-read mixed $is_admin
- * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|array<\Illuminate\Notifications\DatabaseNotification> $notifications
- * @property-read int|null $notifications_count
- * @property-read \Illuminate\Database\Eloquent\Collection|array<\App\Role> $roles
- * @property-read int|null $roles_count
- * @property-read \Illuminate\Database\Eloquent\Collection|array<\Laravel\Passport\Token> $tokens
- * @property-read int|null $tokens_count
- *
- * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
- * @method static \Illuminate\Database\Query\Builder|User onlyTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder|User query()
- * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereGranularity($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereLanguage($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|User withTrashed()
- * @method static \Illuminate\Database\Query\Builder|User withoutTrashed()
- *
- * @mixin \Eloquent
  */
 class User extends Authenticatable implements LdapAuthenticatable
 {
@@ -151,9 +110,15 @@ class User extends Authenticatable implements LdapAuthenticatable
         $this->notify(new ResetPassword($token));
     }
 
+    // Add some caching for roles
+    private $roles = null;
+
     public function roles()
     {
-        return $this->belongsToMany(Role::class)->orderBy('title');
+        if ($this->roles === null)
+            return ($this->roles=$this->belongsToMany(Role::class)->orderBy('title'));
+        else
+            return $this->roles;
     }
 
     public function m_applications()
