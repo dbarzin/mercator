48dc32d29a3284b5e508eda7561f6c2175ad11af
Didier <dbarzin@gmail.com>
2024-01-16 11:28:32 +0100

    code cleanup

--- app/Subnetwork.php
+++ app/Subnetwork.php
@@ -10,50 +10,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 /**
  * App\Subnetwork
  *
- * @property int $id
- * @property string|null $description
- * @property string|null $address
- * @property string|null $ip_range
- * @property string|null $ip_allocation_type
- * @property string|null $responsible_exp
- * @property string|null $dmz
- * @property string|null $wifi
- * @property string $name
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property int|null $connected_subnets_id
- * @property int|null $gateway_id
- *
- * @property-read \Illuminate\Database\Eloquent\Collection|array<Subnetwork> $connectedSubnetsSubnetworks
- * @property-read int|null $connected_subnets_subnetworks_count
- * @property-read Subnetwork|null $connected_subnets
- * @property-read \App\Gateway|null $gateway
- * @property-read \Illuminate\Database\Eloquent\Collection|array<\App\Network> $subnetworksNetworks
- * @property-read int|null $subnetworks_networks_count
- *
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork newQuery()
- * @method static \Illuminate\Database\Query\Builder|Subnetwork onlyTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork query()
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork whereAddress($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork whereConnectedSubnetsId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork whereDmz($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork whereGatewayId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork whereIpAllocationType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork whereIpRange($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork whereResponsibleExp($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Subnetwork whereWifi($value)
- * @method static \Illuminate\Database\Query\Builder|Subnetwork withTrashed()
- * @method static \Illuminate\Database\Query\Builder|Subnetwork withoutTrashed()
- *
- * @mixin \Eloquent
  */
 class Subnetwork extends Model
 {
--- app/Task.php
+++ app/Task.php
@@ -10,30 +10,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 /**
  * App\Task
  *
- * @property int $id
- * @property string $nom
- * @property string|null $description
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property \Illuminate\Support\Carbon|null $deleted_at
- *
- * @property-read \Illuminate\Database\Eloquent\Collection|array<\App\Operation> $operations
- * @property-read int|null $operations_count
- *
- * @method static \Illuminate\Database\Eloquent\Builder|Task newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Task newQuery()
- * @method static \Illuminate\Database\Query\Builder|Task onlyTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder|Task query()
- * @method static \Illuminate\Database\Eloquent\Builder|Task whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Task whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Task whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Task whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Task whereNom($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Task whereUpdatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|Task withTrashed()
- * @method static \Illuminate\Database\Query\Builder|Task withoutTrashed()
- *
- * @mixin \Eloquent
  */
 class Task extends Model
 {
