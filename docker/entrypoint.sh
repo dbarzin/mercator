7267bd9f2ad8c7653bf1bf6386c3262e793dc5d0
Didier <dbarzin@gmail.com>
2024-01-22 07:51:02 +0100

    fix entrypoint.sh in docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,26 +9,10 @@ _wait_for_db_ready() {
         echo "done."
 }
 
-
 cd /var/www/mercator
 . .env
 
 _wait_for_db_ready
-
-# initialisation de la base de données si elle n'existe pas
-if [ ! -s ./sql/db.sqlite ]
-then
-    php artisan --no-interaction migrate --seed
-
-    # génération des clés
-    php artisan key:generate
-
-    # vider le cache
-    php artisan config:clear
-
-    # configurer Passport for Mercator API : https://dbarzin.github.io/mercator/api/
-    php artisan passport:install
-fi
 
 # start application
 php artisan serve --host=0.0.0.0
