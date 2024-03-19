d9afe2b6df0ca1b7d83bd75b4ebad48fced03248
Didier <dbarzin@gmail.com>
2024-03-19 21:58:54 +0100

    update roadmap

--- resources/views/admin/relations/show.blade.php
+++ resources/views/admin/relations/show.blade.php
@@ -1,37 +1,38 @@
 @extends('layouts.admin')
 @section('content')
 
+<div class="form-group">
+    <a class="btn btn-default" href="{{ route('admin.relations.index') }}">
+        {{ trans('global.back_to_list') }}
+    </a>
+
+    <a class="btn btn-success" href="{{ route('admin.report.explore') }}?node=REL_{{$relation->id}}">
+        {{ trans('global.explore') }}
+    </a>
+
+    @can('entity_edit')
+        <a class="btn btn-info" href="{{ route('admin.relations.edit', $relation->id) }}">
+            {{ trans('global.edit') }}
+        </a>
+    @endcan
+
+    @can('entity_delete')
+        <form action="{{ route('admin.relations.destroy', $relation->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+            <input type="hidden" name="_method" value="DELETE">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <input type="submit" class="btn btn-danger" value="{{ trans('global.delete') }}">
+        </form>
+    @endcan
+
+</div>
+
 <div class="card">
     <div class="card-header">
-        {{ trans('global.show') }} {{ trans('cruds.relation.title') }}
+        {{ trans('cruds.relation.title_singular') }}
     </div>
 
     <div class="card-body">
         <div class="form-group">
-            <div class="form-group">
-                <a class="btn btn-default" href="{{ route('admin.relations.index') }}">
-                    {{ trans('global.back_to_list') }}
-                </a>
-
-                <a class="btn btn-success" href="{{ route('admin.report.explore') }}?node=REL_{{$relation->id}}">
-                    {{ trans('global.explore') }}
-                </a>
-
-                @can('entity_edit')
-                    <a class="btn btn-info" href="{{ route('admin.relations.edit', $relation->id) }}">
-                        {{ trans('global.edit') }}
-                    </a>
-                @endcan
-
-                @can('entity_delete')
-                    <form action="{{ route('admin.relations.destroy', $relation->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-                        <input type="hidden" name="_method" value="DELETE">
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                        <input type="submit" class="btn btn-danger" value="{{ trans('global.delete') }}">
-                    </form>
-                @endcan
-                
-            </div>
             <table class="table table-bordered table-striped">
                 <tbody>
                     <tr>
@@ -96,16 +97,18 @@
                     </tr>
                 </tbody>
             </table>
-            <div class="form-group">
-                <a class="btn btn-default" href="{{ route('admin.relations.index') }}">
-                    {{ trans('global.back_to_list') }}
-                </a>
-            </div>
         </div>
     </div>
     <div class="card-footer">
         {{ trans('global.created_at') }} {{ $relation->created_at ? $relation->created_at->format(trans('global.timestamp')) : '' }} |
-        {{ trans('global.updated_at') }} {{ $relation->updated_at ? $relation->updated_at->format(trans('global.timestamp')) : '' }} 
+        {{ trans('global.updated_at') }} {{ $relation->updated_at ? $relation->updated_at->format(trans('global.timestamp')) : '' }}
     </div>
 </div>
+
+<div class="form-group">
+    <a class="btn btn-default" href="{{ route('admin.relations.index') }}">
+        {{ trans('global.back_to_list') }}
+    </a>
+</div>
+
 @endsection
