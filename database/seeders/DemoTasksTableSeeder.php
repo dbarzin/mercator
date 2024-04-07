307bdaa09c87ed7f36aaa07b32c41fa31cc0f5cf
Didier <dbarzin@gmail.com>
2024-04-07 12:55:10 +0200

    fix demo seeders

--- database/seeders/DemoTasksTableSeeder.php
+++ database/seeders/DemoTasksTableSeeder.php
@@ -14,40 +14,40 @@ class DemoTasksTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('tasks')->delete();
-        
+
         \DB::table('tasks')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => 1,
-                'nom' => 'Tâche 2',
-                'description' => 'Descriptionde la tâche 2',
+                'name' => 'Tâche 1',
+                'description' => 'Descriptionde la tâche 1',
                 'created_at' => '2020-06-13 02:04:07',
                 'updated_at' => '2020-06-13 02:04:07',
                 'deleted_at' => NULL,
             ),
-            1 => 
+            1 =>
             array (
                 'id' => 2,
-                'nom' => 'Tache 1',
-                'description' => 'Description de la tâche 1',
+                'name' => 'Tache 2',
+                'description' => 'Description de la tâche 2',
                 'created_at' => '2020-06-13 02:04:21',
                 'updated_at' => '2020-06-13 02:04:21',
                 'deleted_at' => NULL,
             ),
-            2 => 
+            2 =>
             array (
                 'id' => 3,
-                'nom' => 'Tâche 3',
+                'name' => 'Tâche 3',
                 'description' => 'Description de la tâche 3',
                 'created_at' => '2020-06-13 02:04:41',
                 'updated_at' => '2020-06-13 02:04:41',
                 'deleted_at' => NULL,
             ),
         ));
-        
-        
+
+
     }
 }
