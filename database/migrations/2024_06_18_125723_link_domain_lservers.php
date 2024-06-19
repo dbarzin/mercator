6f0c9cb942bfe346fe16e62b6d9edb38cacef8b8
Didier <dbarzin@gmail.com>
2024-06-19 14:06:41 +0200

    add database migration

---
+++ database/migrations/2024_06_18_125723_link_domain_lservers.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('logical_servers', function(Blueprint $table) {
+            $table->unsignedInteger('domain_id')->index('domain_id_fk_493844')->nullable();
+            $table->foreign('domain_id','domain_id_fk_493844')->references('id')->on('domaine_ads')->onUpdate('NO ACTION')->onDelete('SET NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('logical_servers', function (Blueprint $table) {
+            if (DB::getDriverName() !== 'sqlite') {
+                $table->dropForeign('domain_id_fk_493844');
+            }
+        });
+
+        Schema::table('logical_servers', function (Blueprint $table) {
+            $table->dropColumn('domain_id');
+        });
+    }
+};
