3a7f76b571d713a915c4b506ba8bad96608b82a9
didier <dbarzin@gmail.com>
2024-09-26 14:44:04 +0200

    change login page

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,10 +1,37 @@
 @extends('layouts.app')
+@section('styles')
+<style>
+     body {
+                background-image: url('/images/mercator.png');
+                background-size:  800px 800px;
+                background-position: center;
+                background-repeat: no-repeat;
+                background-color: #3b2924;
+            }
+
+        .card {
+            background-color: rgba(255, 255, 255, 0.5); /* Couleur blanche avec 80% d'opacité */
+        }
+
+        .input-group-text {
+            background-color: rgba(255, 255, 255, 0.5); /* Fond partiellement transparent pour les icônes */
+        }
+
+        .form-control {
+            background-color: rgba(255, 255, 255, 0.7); /* Fond partiellement transparent pour les champs de texte */
+        }
+
+        h1 {
+            text-align: center;
+            color: #333; /* Couleur du titre */
+        }
+</style>
+@endsection
 @section('content')
 <div class="row justify-content-center">
     <div class="col-md-6">
         <div class="card mx-4">
             <div class="card-body p-4">
-                <h1>{{ trans('panel.site_title') }}</h1>
 
                 <p class="text-muted">{{ trans('global.reset_password') }}</p>
 
