03d417e5185800c140d6efa9f81763a5f7e43b38
didier <dbarzin@gmail.com>
2024-10-05 22:12:01 +0200

    layouts

--- resources/views/admin/annuaires/create.blade.php
+++ resources/views/admin/annuaires/create.blade.php
@@ -1,14 +1,12 @@
 @extends('layouts.admin')
 @section('content')
-
-<div class="card">
-    <div class="card-header">
-        {{ trans('global.create') }} {{ trans('cruds.annuaire.title_singular') }}
-    </div>
-
-    <div class="card-body">
-        <form method="POST" action="{{ route("admin.annuaires.store") }}" enctype="multipart/form-data">
-            @csrf
+<form method="POST" action="{{ route("admin.annuaires.store") }}" enctype="multipart/form-data">
+    @csrf
+    <div class="card">
+        <div class="card-header">
+            {{ trans('global.create') }} {{ trans('cruds.annuaire.title_singular') }}
+        </div>
+        <div class="card-body">
             <div class="form-group">
                 <label class="required" for="name">{{ trans('cruds.annuaire.fields.name') }}</label>
                 <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', '') }}" required>
@@ -53,14 +51,14 @@
                 @endif
                 <span class="help-block">{{ trans('cruds.annuaire.fields.zone_admin_helper') }}</span>
             </div>
-            <div class="form-group">
-                <button class="btn btn-danger" type="submit">
-                    {{ trans('global.save') }}
-                </button>
-            </div>
-        </form>
+        </div>
     </div>
-</div>
+    <div class="form-group">
+        <button class="btn btn-danger" type="submit">
+            {{ trans('global.save') }}
+        </button>
+    </div>
+</form>
 
 
 
@@ -83,7 +81,7 @@ $(document).ready(function () {
         placeholder: "{{ trans('global.pleaseSelect') }}",
         allowClear: true,
         tags: true
-    }) 
+    })
 
 });
 </script>
--- resources/views/admin/lans/create.blade.php
+++ resources/views/admin/lans/create.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.admin')
 @section('content')
+<form method="POST" action="{{ route("admin.lans.store") }}" enctype="multipart/form-data">
+    @csrf
+    <div class="card">
+        <div class="card-header">
+            {{ trans('global.create') }} {{ trans('cruds.lan.title_singular') }}
+        </div>
 
-<div class="card">
-    <div class="card-header">
-        {{ trans('global.create') }} {{ trans('cruds.lan.title_singular') }}
-    </div>
-
-    <div class="card-body">
-        <form method="POST" action="{{ route("admin.lans.store") }}" enctype="multipart/form-data">
-            @csrf
+        <div class="card-body">
             <div class="form-group">
                 <label class="required" for="name">{{ trans('cruds.lan.fields.name') }}</label>
                 <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', '') }}" required>
@@ -29,14 +28,14 @@
                 @endif
                 <span class="help-block">{{ trans('cruds.lan.fields.description_helper') }}</span>
             </div>
-            <div class="form-group">
-                <button class="btn btn-danger" type="submit">
-                    {{ trans('global.save') }}
-                </button>
-            </div>
-        </form>
+        </div>
     </div>
-</div>
+    <div class="form-group">
+        <button class="btn btn-danger" type="submit">
+            {{ trans('global.save') }}
+        </button>
+    </div>
+</form>
 
 
 
