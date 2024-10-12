587d432d6748792ec9d3e73bd9a435b34939cd46
didier <dbarzin@gmail.com>
2024-10-12 22:55:39 +0200

    layout

--- resources/views/admin/applicationModules/edit.blade.php
+++ resources/views/admin/applicationModules/edit.blade.php
@@ -1,15 +1,14 @@
 @extends('layouts.admin')
 @section('content')
+<form method="POST" action="{{ route("admin.application-modules.update", [$applicationModule->id]) }}" enctype="multipart/form-data">
+    @method('PUT')
+    @csrf
+    <div class="card">
+        <div class="card-header">
+            {{ trans('global.edit') }} {{ trans('cruds.applicationModule.title_singular') }}
+        </div>
 
-<div class="card">
-    <div class="card-header">
-        {{ trans('global.edit') }} {{ trans('cruds.applicationModule.title_singular') }}
-    </div>
-
-    <div class="card-body">
-        <form method="POST" action="{{ route("admin.application-modules.update", [$applicationModule->id]) }}" enctype="multipart/form-data">
-            @method('PUT')
-            @csrf
+        <div class="card-body">
             <div class="form-group">
                 <label class="required" for="name">{{ trans('cruds.applicationModule.fields.name') }}</label>
                 <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', $applicationModule->name) }}" required>
@@ -30,14 +29,14 @@
                 @endif
                 <span class="help-block">{{ trans('cruds.applicationModule.fields.description_helper') }}</span>
             </div>
-            <div class="form-group">
-                <button class="btn btn-danger" type="submit">
-                    {{ trans('global.save') }}
-                </button>
-            </div>
-        </form>
+        </div>
     </div>
-</div>
+    <div class="form-group">
+        <button class="btn btn-danger" type="submit">
+            {{ trans('global.save') }}
+        </button>
+    </div>
+</form>
 @endsection
 
 @section('scripts')
@@ -57,7 +56,7 @@ $(document).ready(function () {
         placeholder: "{{ trans('global.pleaseSelect') }}",
         allowClear: true,
         tags: true
-    }) 
+    })
 
 });
 </script>
