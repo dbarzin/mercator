49ac7fac25365d0958f26c120fde3e71fb35e165
didier <dbarzin@gmail.com>
2024-10-13 21:46:47 +0200

    layout

--- resources/views/doc/guide.blade.php
+++ resources/views/doc/guide.blade.php
@@ -3,23 +3,25 @@
 @section('content')
 <div class="content">
     <div class="row">
-        <div class="col-lg-12">
-            <div class="card">
-                <div class="card-header">
-                    {{ trans("doc.guide.title") }}
+    <div class="card">
+        <div class="card-header">
+            {{ trans("doc.guide.title") }}
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-3">
+                    <img src="/images/mercator.png" class="img-fluid">
                 </div>
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-6" style="min-width: 460px;">
-                        <p>{!! trans("doc.guide.p1") !!}</p>
-                        <p>{!! trans("doc.guide.p2") !!}</p>
-                        <p>{!! trans("doc.guide.p3") !!}</p>
-                        <p>{!! trans("doc.guide.p4") !!}</p>
-                    </div>
-                  </div>
-              </div>
-          </div>
-      </div>
+                <div class="col-6" style="min-width: 460px;">
+                    <p>{!! trans("doc.guide.p1") !!}</p>
+                    <p>{!! trans("doc.guide.p2") !!}</p>
+                    <p>{!! trans("doc.guide.p3") !!}</p>
+                    <p>{!! trans("doc.guide.p4") !!}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
   </div>
 </div>
 @endsection
