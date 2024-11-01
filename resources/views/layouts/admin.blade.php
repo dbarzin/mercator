9d8d39914c51308fbd45079c4981059051ab0ad6
didier <dbarzin@gmail.com>
2024-11-01 12:56:07 +0100

    layout buttons

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -166,7 +166,7 @@
 
     <!-- Bootstrap -->
     <!-- script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script -->
-    <script src="/js/bootstrap.bundle.min.js"></script>
+    <!--script src="/js/bootstrap.bundle.min.js"></script-->
 
     <!-- script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script -->
     <script src="/js/popper.min.js"></script>
