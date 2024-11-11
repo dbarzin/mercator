84bdaa0a652c56600031f7bff1862572aef385e5
didier <dbarzin@gmail.com>
2024-11-11 22:18:52 +0100

    fix API

--- app/Http/Controllers/API/PhysicalServerController.php
+++ app/Http/Controllers/API/PhysicalServerController.php
@@ -27,9 +27,8 @@ class PhysicalServerController extends Controller
         abort_if(Gate::denies('physical_server_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         $physicalserver = PhysicalServer::create($request->all());
-        $physicalserver->applications()->sync($request->input('applications', []));
-        // syncs
-        // $physicalserver->roles()->sync($request->input('roles', []));
+        if ($request->has('applications'))
+            $physicalserver->applications()->sync($request->input('applications', []));
 
         return response()->json($physicalserver, 201);
     }
