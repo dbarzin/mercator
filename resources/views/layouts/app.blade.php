df15211cf29a9891ccf329aadc9c27d9dd06fb30
didier <dbarzin@gmail.com>
2024-12-08 14:54:20 +0100

    improve login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,26 +8,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ trans('panel.site_title') }}</title>
-    <!-- link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" /-->
-    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" />
-    <!--link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" /-->
-    <link href="{{ asset('/css/font-awesome.css') }}" rel="stylesheet" />
-    <!-- link href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" rel="stylesheet" /-->
-    <link href="{{ asset('/css/all.css') }}" rel="stylesheet" />
-    <!--<link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet" /-->
-    <link href="{{ asset('/css/jquery.dataTables.min.css') }}" rel="stylesheet" />
-    <!-- link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet" /-->
-    <link href="{{ asset('/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" />
-    <!-- link href="https://cdn.datatables.net/buttons/1.2.4/css/buttons.dataTables.min.css" rel="stylesheet" /-->
-    <link href="{{ asset('/css/buttons.dataTables.min.css') }}" rel="stylesheet" />
-    <!-- link href="https://cdn.datatables.net/select/1.3.0/css/select.dataTables.min.css" rel="stylesheet" /-->
-    <link href="{{ asset('/css/select.dataTables.min.css') }}" rel="stylesheet" />
-    <!-- link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" rel="stylesheet" /-->
-    <link href="{{ asset('/css/select2.min.css') }}" rel="stylesheet" />
-    <!-- link href="https://unpkg.com/@coreui/coreui@2.1.16/dist/css/coreui.min.css" rel="stylesheet" /-->
-    <link href="{{ asset('/css/coreui.min.css') }}" rel="stylesheet" />
-    <!-- link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet" /-->
 
+    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/css/font-awesome.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/css/all.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/css/coreui.min.css') }}" rel="stylesheet" />
+    
     <!-- custom css -->
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet" />
     @yield('styles')
