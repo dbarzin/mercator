fec25b044286621b01bc3a53c62d727a410d5ff3
didier <dbarzin@gmail.com>
2025-04-09 13:02:26 +0200

    add maxlength

--- resources/views/admin/entities/edit.blade.php
+++ resources/views/admin/entities/edit.blade.php
@@ -13,7 +13,7 @@
             <div class="col-md-4">
                 <div class="form-group">
                     <label class="required" for="name">{{ trans('cruds.entity.fields.name') }}</label>
-                    <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', $entity->name) }}" required>
+                    <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', $entity->name) }}" required maxlength="32">
                     @if($errors->has('name'))
                         <div class="invalid-feedback">
                             {{ $errors->first('name') }}
--- resources/views/admin/information/edit.blade.php
+++ resources/views/admin/information/edit.blade.php
@@ -12,7 +12,7 @@
         <div class="card-body">
             <div class="form-group">
                 <label class="required" for="name">{{ trans('cruds.information.fields.name') }}</label>
-                <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', $information->name) }}" required>
+                <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', $information->name) }}" required maxlength="32">
                 @if($errors->has('name'))
                     <div class="invalid-feedback">
                         {{ $errors->first('name') }}
