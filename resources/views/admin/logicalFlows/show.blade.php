72d3c50d191a6f5ecfe09b5f415fe42935ee81f3
didier <dbarzin@gmail.com>
2025-07-09 21:52:26 +0200

    fix explore link

--- resources/views/admin/logicalFlows/show.blade.php
+++ resources/views/admin/logicalFlows/show.blade.php
@@ -5,7 +5,7 @@
         {{ trans('global.back_to_list') }}
     </a>
 
-    <a class="btn btn-success" href="{{ route('admin.report.explore') }}?flow={{$logicalFlow->id}}">
+    <a class="btn btn-success" href="{{ route('admin.report.explore') }}?node={{$logicalFlow->sourceId()}},{{$logicalFlow->destinationId()}}">
         {{ trans('global.explore') }}
     </a>
 
