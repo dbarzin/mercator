60f192d7216e0e88b000c57be7c441b544a0a1b4
didier <dbarzin@gmail.com>
2025-08-26 07:09:41 +0200

    remove debug code

--- app/Http/Controllers/Admin/CartographyController.php
+++ app/Http/Controllers/Admin/CartographyController.php
@@ -2583,7 +2583,7 @@ class CartographyController extends Controller
         // Save it to a file
         $dotPath = tempnam('/tmp', 'dot');
 
-        \Log::Debug("generateGraphImage in {$dotPath}");
+        // \Log::Debug("generateGraphImage in {$dotPath}");
 
         $dotFile = fopen($dotPath, 'w');
         fwrite($dotFile, $graph);
@@ -2593,15 +2593,14 @@ class CartographyController extends Controller
         $imagePath = tempnam('/tmp', 'png');
 
         // Call DOT : dot -Tpng ./test.dot -otest.png
-        \Log::Debug("call /usr/bin/dot -Tpng -Gdpi=300 {$dotPath} -o{$imagePath}");
+        // \Log::Debug("call /usr/bin/dot -Tpng -Gdpi=300 {$dotPath} -o{$imagePath}");
 
         // add "unset SERVER_NAME;" due to Apache2
         // see: https://github.com/glejeune/Ruby-Graphviz/issues/69
         shell_exec("unset SERVER_NAME; /usr/bin/dot -Tpng -Gdpi=300 {$dotPath} -o{$imagePath}");
 
         // delete graph file
-        // TODO : add unlink - removed for debug
-        // unlink($dotPath);
+        unlink($dotPath);
 
         // Test for SVG (not supported)
         // $this->embedImagesInSvg($imagePath);
