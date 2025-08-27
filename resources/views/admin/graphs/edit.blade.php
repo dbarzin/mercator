095a98e31fd7bef2f96ad7a60e15b35c42747d55
didier <dbarzin@gmail.com>
2025-08-27 10:33:23 +0200

    work on maps

--- resources/views/admin/graphs/edit.blade.php
+++ resources/views/admin/graphs/edit.blade.php
@@ -186,7 +186,7 @@
         <a id="btn-cancel" class="btn btn-default" href="{{ route('admin.graphs.index') }}">
             {{ trans('global.back_to_list') }}
         </a>
-        <button id="btn-save" class="btn btn-danger" type="submit" id="submitButton">
+        <button id="btn-save" class="btn btn-danger" type="submit">
             {{ trans('global.save') }}
         </button>
     </div>
