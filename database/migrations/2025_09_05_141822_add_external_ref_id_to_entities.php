b12e4fb0ab57703e8f971f792543d0319903ac01
didier <dbarzin@gmail.com>
2025-09-05 14:21:59 +0200

    add external ref id to entities

---
+++ database/migrations/2025_09_05_141822_add_external_ref_id_to_entities.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('entities', function (Blueprint $table) {
+            // Field added to sync tables with external referentials
+            // used by U-Lille to sync with SINAPS
+            $table->string('external_ref_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('entities', function (Blueprint $table) {
+            $table->dropColumn('external_ref_id');
+        });
+    }
+};
