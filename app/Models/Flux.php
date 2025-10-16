b4ad19d2df6760f108bd4c7d65e84f6be08ac57b
dbarzin <dbarzin@gmail.com>
2025-10-16 18:55:19 +0200

    Remove HasFactory

--- app/Models/Flux.php
+++ app/Models/Flux.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use App\Traits\Auditable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -13,7 +12,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class Flux extends Model
 {
-    use Auditable, HasFactory, SoftDeletes;
+    use Auditable, SoftDeletes;
 
     public $table = 'fluxes';
 
