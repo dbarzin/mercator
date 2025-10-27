275cd10105415c50637d750baf2a2edfd12da755
dbarzin <dbarzin@gmail.com>
2025-10-27 17:40:44 +0100

    extend application service name size

--- resources/views/admin/applicationServices/edit.blade.php
+++ resources/views/admin/applicationServices/edit.blade.php
@@ -1,98 +1,112 @@
 @extends('layouts.admin')
 @section('content')
-<form method="POST" action="{{ route("admin.application-services.update", [$applicationService->id]) }}" enctype="multipart/form-data">
-    @method('PUT')
-    @csrf
-    <div class="card">
-        <div class="card-header">
-            {{ trans('global.edit') }} {{ trans('cruds.applicationService.title_singular') }}
-        </div>
-        <div class="card-body">
-            <div class="form-group">
-                <label class="required" for="name">{{ trans('cruds.applicationService.fields.name') }}</label>
-                <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', $applicationService->name) }}" required autofocus/>
-                @if($errors->has('name'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('name') }}
-                    </div>
-                @endif
-                <span class="help-block">{{ trans('cruds.applicationService.fields.name_helper') }}</span>
+    <form method="POST" action="{{ route("admin.application-services.update", [$applicationService->id]) }}"
+          enctype="multipart/form-data">
+        @method('PUT')
+        @csrf
+        <div class="card">
+            <div class="card-header">
+                {{ trans('global.edit') }} {{ trans('cruds.applicationService.title_singular') }}
             </div>
-            <div class="form-group">
-                <label class="recommended" for="description">{{ trans('cruds.applicationService.fields.description') }}</label>
-                <textarea class="form-control ckeditor {{ $errors->has('description') ? 'is-invalid' : '' }}" name="description" id="description">{!! old('description', $applicationService->description) !!}</textarea>
-                @if($errors->has('description'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('description') }}
-                    </div>
-                @endif
-                <span class="help-block">{{ trans('cruds.applicationService.fields.description_helper') }}</span>
-            </div>
-
-            <div class="form-group">
-                <label for="exposition">{{ trans('cruds.applicationService.fields.exposition') }}</label>
-                <select class="form-control select2-free {{ $errors->has('responsible') ? 'is-invalid' : '' }}" name="exposition" id="exposition">
-                    @if (!$exposition_list->contains(old('exposition')))
-                        <option> {{ old('exposition') }}</option>'
+            <div class="card-body">
+                <div class="form-group">
+                    <label class="required" for="name">{{ trans('cruds.applicationService.fields.name') }}</label>
+                    <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name"
+                           id="name" value="{{ old('name', $applicationService->name) }}" maxlength="64" required
+                           autofocus/>
+                    @if($errors->has('name'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('name') }}
+                        </div>
                     @endif
-                    @foreach($exposition_list as $t)
-                        <option {{ (old('exposition') ? old('exposition') : $applicationService->exposition) == $t ? 'selected' : '' }}>{{$t}}</option>
-                    @endforeach
-                </select>
-                @if($errors->has('exposition'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('exposition') }}
-                    </div>
-                @endif
-                <span class="help-block">{{ trans('cruds.applicationService.fields.exposition_helper') }}</span>
-            </div>
-
-            <div class="form-group">
-                <label class="recommended" for="applications">{{ trans('cruds.applicationService.fields.applications') }}</label>
-                <div style="padding-bottom: 4px">
-                    <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
-                    <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
+                    <span class="help-block">{{ trans('cruds.applicationService.fields.name_helper') }}</span>
                 </div>
-                <select class="form-control select2 {{ $errors->has('applications') ? 'is-invalid' : '' }}" name="applications[]" id="applications" multiple>
-                    @foreach($applications as $id => $applications)
-                        <option value="{{ $id }}" {{ (in_array($id, old('applications', [])) || $applicationService->applications->contains($id)) ? 'selected' : '' }}>{{ $applications }}</option>
-                    @endforeach
-                </select>
-                @if($errors->has('applications'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('applications') }}
-                    </div>
-                @endif
-                <span class="help-block">{{ trans('cruds.applicationService.fields.applications_helper') }}</span>
-            </div>
-
-            <div class="form-group">
-                <label for="modules">{{ trans('cruds.applicationService.fields.modules') }}</label>
-                <div style="padding-bottom: 4px">
-                    <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
-                    <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
+                <div class="form-group">
+                    <label class="recommended"
+                           for="description">{{ trans('cruds.applicationService.fields.description') }}</label>
+                    <textarea class="form-control ckeditor {{ $errors->has('description') ? 'is-invalid' : '' }}"
+                              name="description"
+                              id="description">{!! old('description', $applicationService->description) !!}</textarea>
+                    @if($errors->has('description'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('description') }}
+                        </div>
+                    @endif
+                    <span class="help-block">{{ trans('cruds.applicationService.fields.description_helper') }}</span>
                 </div>
-                <select class="form-control select2 {{ $errors->has('modules') ? 'is-invalid' : '' }}" name="modules[]" id="modules" multiple>
-                    @foreach($modules as $id => $modules)
-                        <option value="{{ $id }}" {{ (in_array($id, old('modules', [])) || $applicationService->modules->contains($id)) ? 'selected' : '' }}>{{ $modules }}</option>
-                    @endforeach
-                </select>
-                @if($errors->has('modules'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('modules') }}
+
+                <div class="form-group">
+                    <label for="exposition">{{ trans('cruds.applicationService.fields.exposition') }}</label>
+                    <select class="form-control select2-free {{ $errors->has('responsible') ? 'is-invalid' : '' }}"
+                            name="exposition" id="exposition">
+                        @if (!$exposition_list->contains(old('exposition')))
+                            <option> {{ old('exposition') }}</option>'
+                        @endif
+                        @foreach($exposition_list as $t)
+                            <option {{ (old('exposition') ? old('exposition') : $applicationService->exposition) == $t ? 'selected' : '' }}>{{$t}}</option>
+                        @endforeach
+                    </select>
+                    @if($errors->has('exposition'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('exposition') }}
+                        </div>
+                    @endif
+                    <span class="help-block">{{ trans('cruds.applicationService.fields.exposition_helper') }}</span>
+                </div>
+
+                <div class="form-group">
+                    <label class="recommended"
+                           for="applications">{{ trans('cruds.applicationService.fields.applications') }}</label>
+                    <div style="padding-bottom: 4px">
+                        <span class="btn btn-info btn-xs select-all"
+                              style="border-radius: 0">{{ trans('global.select_all') }}</span>
+                        <span class="btn btn-info btn-xs deselect-all"
+                              style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
                     </div>
-                @endif
-                <span class="help-block">{{ trans('cruds.applicationService.fields.modules_helper') }}</span>
+                    <select class="form-control select2 {{ $errors->has('applications') ? 'is-invalid' : '' }}"
+                            name="applications[]" id="applications" multiple>
+                        @foreach($applications as $id => $applications)
+                            <option value="{{ $id }}" {{ (in_array($id, old('applications', [])) || $applicationService->applications->contains($id)) ? 'selected' : '' }}>{{ $applications }}</option>
+                        @endforeach
+                    </select>
+                    @if($errors->has('applications'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('applications') }}
+                        </div>
+                    @endif
+                    <span class="help-block">{{ trans('cruds.applicationService.fields.applications_helper') }}</span>
+                </div>
+
+                <div class="form-group">
+                    <label for="modules">{{ trans('cruds.applicationService.fields.modules') }}</label>
+                    <div style="padding-bottom: 4px">
+                        <span class="btn btn-info btn-xs select-all"
+                              style="border-radius: 0">{{ trans('global.select_all') }}</span>
+                        <span class="btn btn-info btn-xs deselect-all"
+                              style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
+                    </div>
+                    <select class="form-control select2 {{ $errors->has('modules') ? 'is-invalid' : '' }}"
+                            name="modules[]" id="modules" multiple>
+                        @foreach($modules as $id => $modules)
+                            <option value="{{ $id }}" {{ (in_array($id, old('modules', [])) || $applicationService->modules->contains($id)) ? 'selected' : '' }}>{{ $modules }}</option>
+                        @endforeach
+                    </select>
+                    @if($errors->has('modules'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('modules') }}
+                        </div>
+                    @endif
+                    <span class="help-block">{{ trans('cruds.applicationService.fields.modules_helper') }}</span>
+                </div>
             </div>
-    </div>
-</div>
-    <div class="form-group">
-        <a id="btn-cancel" class="btn btn-default" href="{{ route('admin.application-services.index') }}">
-            {{ trans('global.back_to_list') }}
-        </a>
-        <button id="btn-save" class="btn btn-danger" type="submit">
-            {{ trans('global.save') }}
-        </button>
-    </div>
-</form>
+        </div>
+        <div class="form-group">
+            <a id="btn-cancel" class="btn btn-default" href="{{ route('admin.application-services.index') }}">
+                {{ trans('global.back_to_list') }}
+            </a>
+            <button id="btn-save" class="btn btn-danger" type="submit">
+                {{ trans('global.save') }}
+            </button>
+        </div>
+    </form>
 @endsection
