f8f2eb77ab4ff0c96437929284b3026fa47170aa
dbarzin <dbarzin@gmail.com>
2025-10-28 23:22:14 +0100

    phpstan

---
+++ app/Http/Resources/Admin/LogicalFlowResource.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Resources\Admin;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LogicalFlowResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
