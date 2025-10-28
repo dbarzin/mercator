872c6af946fac576719e613a88e94d8cd13c7bfc
dbarzin <dbarzin@gmail.com>
2025-10-28 21:06:08 +0100

    npm build

---
+++ app/Http/Resources/Admin/DataProcessingResource.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Resources\Admin;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DataProcessingResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
