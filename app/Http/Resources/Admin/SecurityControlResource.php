5a97e2e887a60a377b202783e44990fca6a069bf
dbarzin <dbarzin@gmail.com>
2025-10-29 18:11:46 +0100

    Larastan

---
+++ app/Http/Resources/Admin/SecurityControlResource.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Resources\Admin;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SecurityControlResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
