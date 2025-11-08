3977dbf53cc57ad41521fe746b408605f5524424
dbarzin <dbarzin@gmail.com>
2025-11-08 09:37:47 +0100

    review maturity levels

--- resources/views/admin/zoneAdmins/edit.blade.php
+++ resources/views/admin/zoneAdmins/edit.blade.php
@@ -1,42 +1,47 @@
 @extends('layouts.admin')
 @section('content')
-<form method="POST" action="{{ route("admin.zone-admins.update", [$zoneAdmin->id]) }}" enctype="multipart/form-data">
-    @method('PUT')
-    @csrf
-    <div class="card">
-        <div class="card-header">
-            {{ trans('global.edit') }} {{ trans('cruds.zoneAdmin.title_singular') }}
-        </div>
-        <div class="card-body">
-            <div class="form-group">
-                <label class="required" for="name">{{ trans('cruds.zoneAdmin.fields.name') }}</label>
-                <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', $zoneAdmin->name) }}" required autofocus/>
-                @if($errors->has('name'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('name') }}
-                    </div>
-                @endif
-                <span class="help-block">{{ trans('cruds.zoneAdmin.fields.name_helper') }}</span>
+    <form method="POST" action="{{ route("admin.zone-admins.update", [$zoneAdmin->id]) }}"
+          enctype="multipart/form-data">
+        @method('PUT')
+        @csrf
+        <div class="card">
+            <div class="card-header">
+                {{ trans('global.edit') }} {{ trans('cruds.zoneAdmin.title_singular') }}
             </div>
-            <div class="form-group">
-                <label for="description">{{ trans('cruds.zoneAdmin.fields.description') }}</label>
-                <textarea class="form-control ckeditor {{ $errors->has('description') ? 'is-invalid' : '' }}" name="description" id="description">{!! old('description', $zoneAdmin->description) !!}</textarea>
-                @if($errors->has('description'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('description') }}
-                    </div>
-                @endif
-                <span class="help-block">{{ trans('cruds.zoneAdmin.fields.description_helper') }}</span>
+            <div class="card-body">
+                <div class="form-group">
+                    <label class="required" for="name">{{ trans('cruds.zoneAdmin.fields.name') }}</label>
+                    <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name"
+                           id="name" value="{{ old('name', $zoneAdmin->name) }}" required autofocus/>
+                    @if($errors->has('name'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('name') }}
+                        </div>
+                    @endif
+                    <span class="help-block">{{ trans('cruds.zoneAdmin.fields.name_helper') }}</span>
+                </div>
+                <div class="form-group">
+                    <label class="recommended1"
+                           for="description">{{ trans('cruds.zoneAdmin.fields.description') }}</label>
+                    <textarea class="form-control ckeditor {{ $errors->has('description') ? 'is-invalid' : '' }}"
+                              name="description"
+                              id="description">{!! old('description', $zoneAdmin->description) !!}</textarea>
+                    @if($errors->has('description'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('description') }}
+                        </div>
+                    @endif
+                    <span class="help-block">{{ trans('cruds.zoneAdmin.fields.description_helper') }}</span>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="form-group">
-        <a id="btn-cancel" class="btn btn-default" href="{{ route('admin.zone-admins.index') }}">
-            {{ trans('global.back_to_list') }}
-        </a>
-        <button id="btn-save" class="btn btn-danger" type="submit">
-            {{ trans('global.save') }}
-        </button>
-    </div>
-</form>
+        <div class="form-group">
+            <a id="btn-cancel" class="btn btn-default" href="{{ route('admin.zone-admins.index') }}">
+                {{ trans('global.back_to_list') }}
+            </a>
+            <button id="btn-save" class="btn btn-danger" type="submit">
+                {{ trans('global.save') }}
+            </button>
+        </div>
+    </form>
 @endsection
