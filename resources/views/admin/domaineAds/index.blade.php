b7816596704eda6c645bbbff807c4c17706571eb
dbarzin <dbarzin@gmail.com>
2025-11-08 09:37:47 +0100

    review maturity levels

--- resources/views/admin/domaineAds/index.blade.php
+++ resources/views/admin/domaineAds/index.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.admin')
 @section('content')
-@can('domaine_ad_create')
-    <div style="margin-bottom: 10px;" class="row">
-        <div class="col-lg-12">
-            <a id="btn-new" class="btn btn-success" href="{{ route("admin.domaine-ads.create") }}">
-                {{ trans('global.add') }} {{ trans('cruds.domaineAd.title_singular') }}
-            </a>
+    @can('domaine_ad_create')
+        <div style="margin-bottom: 10px;" class="row">
+            <div class="col-lg-12">
+                <a id="btn-new" class="btn btn-success" href="{{ route("admin.domaine-ads.create") }}">
+                    {{ trans('global.add') }} {{ trans('cruds.domaineAd.title_singular') }}
+                </a>
+            </div>
         </div>
-    </div>
-@endcan
-<div class="card">
-    <div class="card-header">
-        {{ trans('cruds.domaineAd.title_singular') }} {{ trans('global.list') }}
-    </div>
+    @endcan
+    <div class="card">
+        <div class="card-header">
+            {{ trans('cruds.domaineAd.title_singular') }} {{ trans('global.list') }}
+        </div>
 
-    <div class="card-body">
-        <div class="table-responsive">
-            <table id="dataTable" class="table table-bordered table-striped table-hover datatable">
-                <thead>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="dataTable" class="table table-bordered table-striped table-hover datatable">
+                    <thead>
                     <tr>
                         <th width="10">
 
@@ -44,10 +44,20 @@
                             &nbsp;
                         </th>
                     </tr>
-                </thead>
-                <tbody>
+                    </thead>
+                    <tbody>
                     @foreach($domaineAds as $domaineAd)
-                        <tr data-entry-id="{{ $domaineAd->id }}">
+                        <tr data-entry-id="{{ $domaineAd->id }}"
+                            @if (
+                                ($domaineAd->description===null)||
+                                ($domaineAd->domain_ctrl_cnt===null)||
+                                ($domaineAd->user_count===null)||
+                                ($domaineAd->machine_count===null)||
+                                ($domaineAd->relation_inter_domaine==null)
+                                )
+                                class="table-warning"
+                                @endif
+                        >
                             <td>
 
                             </td>
@@ -59,11 +69,11 @@
                             <td>
                                 @foreach($domaineAd->forestAds as $forestAd)
                                     <a href="{{ route('admin.forest-ads.show', $forestAd->id) }}">
-                                    {{ $forestAd->name }}
+                                        {{ $forestAd->name }}
                                     </a>
-                                @if ($loop->last!=$forestAd)
-                                ,
-                                @endif
+                                    @if ($loop->last!=$forestAd)
+                                        ,
+                                    @endif
                                 @endforeach
                             </td>
                             <td>
@@ -80,41 +90,46 @@
                             </td>
                             <td nowrap>
                                 @can('domaine_ad_show')
-                                    <a class="btn btn-xs btn-primary" href="{{ route('admin.domaine-ads.show', $domaineAd->id) }}">
+                                    <a class="btn btn-xs btn-primary"
+                                       href="{{ route('admin.domaine-ads.show', $domaineAd->id) }}">
                                         {{ trans('global.view') }}
                                     </a>
                                 @endcan
 
                                 @can('domaine_ad_edit')
-                                    <a class="btn btn-xs btn-info" href="{{ route('admin.domaine-ads.edit', $domaineAd->id) }}">
+                                    <a class="btn btn-xs btn-info"
+                                       href="{{ route('admin.domaine-ads.edit', $domaineAd->id) }}">
                                         {{ trans('global.edit') }}
                                     </a>
                                 @endcan
 
                                 @can('domaine_ad_delete')
-                                    <form action="{{ route('admin.domaine-ads.destroy', $domaineAd->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+                                    <form action="{{ route('admin.domaine-ads.destroy', $domaineAd->id) }}"
+                                          method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');"
+                                          style="display: inline-block;">
                                         <input type="hidden" name="_method" value="DELETE">
                                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
+                                        <input type="submit" class="btn btn-xs btn-danger"
+                                               value="{{ trans('global.delete') }}">
                                     </form>
                                 @endcan
                             </td>
                         </tr>
                     @endforeach
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
 @endsection
 @section('scripts')
-@parent
-<script>
-@include('partials.datatable', array(
-    'id' => '#dataTable',
-    'title' => trans("cruds.domaineAd.title_singular"),
-    'URL' => route('admin.domaine-ads.massDestroy'),
-    'canDelete' => auth()->user()->can('domaine_ad_delete') ? true : false
-));
-</script>
+    @parent
+    <script>
+        @include('partials.datatable', array(
+            'id' => '#dataTable',
+            'title' => trans("cruds.domaineAd.title_singular"),
+            'URL' => route('admin.domaine-ads.massDestroy'),
+            'canDelete' => auth()->user()->can('domaine_ad_delete') ? true : false
+        ));
+    </script>
 @endsection
--- resources/views/admin/forestAds/index.blade.php
+++ resources/views/admin/forestAds/index.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.admin')
 @section('content')
-@can('forest_ad_create')
-    <div style="margin-bottom: 10px;" class="row">
-        <div class="col-lg-12">
-            <a id="btn-new" class="btn btn-success" href="{{ route("admin.forest-ads.create") }}">
-                {{ trans('global.add') }} {{ trans('cruds.forestAd.title_singular') }}
-            </a>
+    @can('forest_ad_create')
+        <div style="margin-bottom: 10px;" class="row">
+            <div class="col-lg-12">
+                <a id="btn-new" class="btn btn-success" href="{{ route("admin.forest-ads.create") }}">
+                    {{ trans('global.add') }} {{ trans('cruds.forestAd.title_singular') }}
+                </a>
+            </div>
         </div>
-    </div>
-@endcan
-<div class="card">
-    <div class="card-header">
-        {{ trans('cruds.forestAd.title_singular') }} {{ trans('global.list') }}
-    </div>
+    @endcan
+    <div class="card">
+        <div class="card-header">
+            {{ trans('cruds.forestAd.title_singular') }} {{ trans('global.list') }}
+        </div>
 
-    <div class="card-body">
-        <div class="table-responsive">
-            <table id="dataTable" class="table table-bordered table-striped table-hover datatable">
-                <thead>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="dataTable" class="table table-bordered table-striped table-hover datatable">
+                    <thead>
                     <tr>
                         <th width="10">
 
@@ -38,16 +38,23 @@
                             &nbsp;
                         </th>
                     </tr>
-                </thead>
-                <tbody>
+                    </thead>
+                    <tbody>
                     @foreach($forestAds as $key => $forestAd)
-                        <tr data-entry-id="{{ $forestAd->id }}">
+                        <tr data-entry-id="{{ $forestAd->id }}"
+                            @if (
+                                ($forestAd->description===null)||
+                                ($forestAd->zone_admin_id===null)
+                                )
+                                class="table-warning"
+                                @endif
+                        >
                             <td>
 
                             </td>
                             <td>
                                 <a href="{{ route('admin.forest-ads.show', $forestAd->id) }}">
-                                {{ $forestAd->name ?? '' }}
+                                    {{ $forestAd->name ?? '' }}
                                 </a>
                             </td>
                             <td>
@@ -55,9 +62,9 @@
                             </td>
                             <td>
                                 @if ($forestAd->zone_admin!=null)
-                                <a href="{{ route('admin.zone-admins.show', $forestAd->zone_admin->id) }}">
-                                    {{ $forestAd->zone_admin->name ?? '' }}
-                                </a>
+                                    <a href="{{ route('admin.zone-admins.show', $forestAd->zone_admin->id) }}">
+                                        {{ $forestAd->zone_admin->name ?? '' }}
+                                    </a>
                                 @endif
                             </td>
                             <td>
@@ -72,22 +79,27 @@
                             </td>
                             <td nowrap>
                                 @can('forest_ad_show')
-                                    <a class="btn btn-xs btn-primary" href="{{ route('admin.forest-ads.show', $forestAd->id) }}">
+                                    <a class="btn btn-xs btn-primary"
+                                       href="{{ route('admin.forest-ads.show', $forestAd->id) }}">
                                         {{ trans('global.view') }}
                                     </a>
                                 @endcan
 
                                 @can('forest_ad_edit')
-                                    <a class="btn btn-xs btn-info" href="{{ route('admin.forest-ads.edit', $forestAd->id) }}">
+                                    <a class="btn btn-xs btn-info"
+                                       href="{{ route('admin.forest-ads.edit', $forestAd->id) }}">
                                         {{ trans('global.edit') }}
                                     </a>
                                 @endcan
 
                                 @can('forest_ad_delete')
-                                    <form action="{{ route('admin.forest-ads.destroy', $forestAd->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+                                    <form action="{{ route('admin.forest-ads.destroy', $forestAd->id) }}" method="POST"
+                                          onsubmit="return confirm('{{ trans('global.areYouSure') }}');"
+                                          style="display: inline-block;">
                                         <input type="hidden" name="_method" value="DELETE">
                                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
+                                        <input type="submit" class="btn btn-xs btn-danger"
+                                               value="{{ trans('global.delete') }}">
                                     </form>
                                 @endcan
 
@@ -95,20 +107,20 @@
 
                         </tr>
                     @endforeach
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
 @endsection
 @section('scripts')
-@parent
-<script>
-@include('partials.datatable', array(
-    'id' => '#dataTable',
-    'title' => trans("cruds.forestAd.title_singular"),
-    'URL' => route('admin.forest-ads.massDestroy'),
-    'canDelete' => auth()->user()->can('forest_ad_delete') ? true : false
-));
-</script>
+    @parent
+    <script>
+        @include('partials.datatable', array(
+            'id' => '#dataTable',
+            'title' => trans("cruds.forestAd.title_singular"),
+            'URL' => route('admin.forest-ads.massDestroy'),
+            'canDelete' => auth()->user()->can('forest_ad_delete') ? true : false
+        ));
+    </script>
 @endsection
