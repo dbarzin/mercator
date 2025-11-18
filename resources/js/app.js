8317f31b54245878317eed5563c606e14182d1d2
dbarzin <dbarzin@gmail.com>
2025-11-18 19:04:36 +0100

    build

--- resources/js/app.js
+++ resources/js/app.js
@@ -34,15 +34,14 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 // DropZone
 import Dropzone from 'dropzone';
 import "dropzone/dist/dropzone.css";
-// Import fonts
-import pdfMake from 'pdfmake/build/pdfmake';
+// Import fonts for pdfMake
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 
 DataTable.Buttons.jszip(jszip);
 DataTable.Buttons.pdfMake(pdfmake);
 
 // Save fonts
-pdfMake.vfs = pdfFonts.vfs ?? pdfFonts.default?.vfs;
+pdfmake.vfs = pdfFonts.vfs ?? pdfFonts.default?.vfs;
 
 // Initialize select2
 select2($);
