4c599e72c984ff228537145b7ffd8aa015e08601
dbarzin <dbarzin@gmail.com>
2025-11-28 11:10:18 +0100

    module name 64 caracters + yellow on index

--- app/Http/Requests/StoreApplicationModuleRequest.php
+++ app/Http/Requests/StoreApplicationModuleRequest.php
@@ -21,7 +21,7 @@ class StoreApplicationModuleRequest extends FormRequest
         return [
             'name' => [
                 'min:3',
-                'max:32',
+                'max:64',
                 'required',
                 Rule::unique('application_modules')->whereNull('deleted_at'),
             ],
