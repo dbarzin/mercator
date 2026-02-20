839d231399944e43a865198262e96e0218252cc3
dbarzin <dbarzin@gmail.com>
2026-02-20 10:16:01 +0100

    XSS protection on Entities

--- app/Http/Requests/StoreEntityRequest.php
+++ app/Http/Requests/StoreEntityRequest.php
@@ -3,20 +3,21 @@
 namespace App\Http\Requests;
 
 use Gate;
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 use Symfony\Component\HttpFoundation\Response;
 
-class StoreEntityRequest extends FormRequest
+class StoreEntityRequest extends BaseFormRequest
 {
-    public function authorize()
+    protected array $htmlFields = ['description', 'security_level', 'contact_point'];
+
+    public function authorize(): bool
     {
         abort_if(Gate::denies('entity_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         return true;
     }
 
-    public function rules()
+    public function rules(): array
     {
         return [
             'name' => [
@@ -29,8 +30,8 @@ class StoreEntityRequest extends FormRequest
             'seurity_level' => [
                 'nullable',
                 'integer',
-                'min:-2147483648',
-                'max:2147483647',
+                'min:0',
+                'max:5',
             ],
         ];
     }
