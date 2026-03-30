8421790ae37dc4d426dbf111e52609dec3af79a7
dbarzin <dbarzin@gmail.com>
2026-03-30 10:57:51 +0200

    fix typo in show role

--- resources/views/admin/roles/show.blade.php
+++ resources/views/admin/roles/show.blade.php
@@ -2591,7 +2591,7 @@
                 <div class="col-md-2">
                     <div class="form-group">
                         <label>{{ trans('cruds.configuration.documents.title') }}</label>
-                        @php($permission = $permissions_sorted['documento'])
+                        @php($permission = $permissions_sorted['document'])
                         <div class="form-switch">
                             <input class="form-check-input" type="checkbox" disabled name="permissions[]"
                                    data-check="{{ $permission['name'] }}" id="perm_{{ $permission['actions'][4][0] }}"
