0bd785bbb199d50770b49947190358d76effc437
dbarzin <dbarzin@gmail.com>
2026-04-17 10:27:20 +0200

    fix

--- resources/js/graphviz.js
+++ resources/js/graphviz.js
@@ -1,8 +1,14 @@
+// Graphviz (WASM)
 import {Graphviz} from "@hpcc-js/wasm-graphviz";
 
-Graphviz.load().then((graphviz) => {
-    window.graphviz = graphviz;
-    document.dispatchEvent(new Event('graphvizReady'));
-});
-
-
+window.graphvizReady = Graphviz.load()
+    .then((graphviz) => {
+        window.graphviz = graphviz;
+        document.dispatchEvent(new Event('graphvizReady'));
+        return graphviz;
+    })
+    .catch((err) => {
+        console.error('Failed to load Graphviz WASM:', err);
+        document.dispatchEvent(new CustomEvent('graphvizError', {detail: err}));
+        throw err;
+    });
