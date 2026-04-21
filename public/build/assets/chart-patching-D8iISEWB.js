33d32fea7513787cfac758a97a3649de6c39aa1d
dbarzin <dbarzin@gmail.com>
2026-04-21 14:57:50 +0200

    rebuild

---
+++ public/build/assets/chart-patching-D8iISEWB.js
@@ -0,0 +1,2 @@
+import{C as r,B as a,l as e,A as o,h as t,j as l,L as s}from"./chart-BZ59Wa_3.js";import{p as n}from"./chartjs-plugin-datalabels.esm-Csg78G89.js";r.register(a,e,o,t,l,s,n);window.Chart=r;
+//# sourceMappingURL=chart-patching-D8iISEWB.js.map
